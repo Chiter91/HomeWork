0ecc3a5b3d79eca295c8733c6542e44fb9edbbbd
Chiter <chinaric@tut.by>
2019-12-03 23:21:19 +0200

    Commit

--- src/assessment/assessment7/MainClass.java
+++ src/assessment/assessment7/MainClass.java
@@ -2,23 +2,25 @@ package assessment.assessment7;
 
 public class MainClass {
     public static void main(String[] args) {
-        Pair<Integer, String> p1 = new Pair<>(11, "Hello World");
+//        PairOld<Integer, String> p1 = new PairOld<>(11, "Hello World");
+//        System.out.println(p1.first() + " _ " + p1.last());
+//        p1.swap();
+//        System.out.println(p1.first() + " _ " + p1.last());
+//        p1.replaceFirst(99);
+//        p1.replaceLast("QWERTY");
+//        System.out.println(p1.first() + " _ " + p1.last());
+//        p1.swap();
+//        System.out.println(p1.first() + " _ " + p1.last());
 
-        System.out.println(p1.first() + " _ " + p1.last());
+        Pair<Integer, String> p2 = new Pair<>(11, "Hello World");
 
-        p1.swap();
-
-        System.out.println(p1.first() + " _ " + p1.last());
-
-        p1.replaceFirst(99);
-
-        p1.replaceLast("QWERTY");
-
-        System.out.println(p1.first() + " _ " + p1.last());
-
-        p1.swap();
-
-        System.out.println(p1.first() + " _ " + p1.last());
+        System.out.println("Первый элемент: " + p2.first());
+        System.out.println("------------------------");
+        System.out.println("Второй элемент: " + p2.last());
+        System.out.println("------------------------");
+        System.out.println("Первый элемент: " + p2.swap().first());
+        System.out.println("------------------------");
+        System.out.println("Первый элемент: " + p2.first());
 
     }
 }
--- src/assessment/assessment7/PairOld.java
+++ src/assessment/assessment7/PairOld.java
@@ -11,26 +11,27 @@ replaceFirst() - заменяет 1ый элемент на новый
 replaceLast() - заменяет 2ой элемент на новый
  */
 
-public class Pair<T, K> {
-    private T value1;
-    private K value2;
+public class PairOld<T, K> {
+    private Object value1;
+    private Object value2;
 
-    public Pair(T value1, K value2) {
+    public PairOld(T value1, K value2) {
         this.value1 = value1;
         this.value2 = value2;
     }
 
-    public T first () {
+    public Object first () {
         return value1;
     }
 
-    public K last () {
+    public Object last () {
         return value2;
     }
 
-    public Pair<K, T> swap() {
-
-        return new Pair<>(value2, value1);
+    public void swap() {
+        Object temp = value1;
+        value1 = value2;
+        value2 = temp;
     }
 
     public void replaceFirst(T newValue) {
